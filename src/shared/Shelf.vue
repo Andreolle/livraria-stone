02d56f9f8104defa20a7654a94d3389eef48e593
Andreolle Dantas <andreolle18@gmail.com>
2017-11-15 20:33:21 -0200

    Exibindo preço quando existe

--- src/shared/Shelf.vue
+++ src/shared/Shelf.vue
@@ -5,35 +5,57 @@
 		</div>
 		
 		<div class="shelf__content">
-			<h2 class="shelf__title">{{ title }}</h2>
-			<p class="shelf__description">{{ description }}</p>
-			<p class="shelf__price">{{ price }}</p>
+			<h2 class="shelf__title">{{ title | truncate(30) }}</h2>
+			<p class="shelf__description">{{ description | truncate(40) }}</p>
+			<p class="shelf__price">{{ price | currentConverter }}</p>
 		</div>
     </div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
+	props: ['title', 'description', 'price', 'img'],
+	filters: {
+		truncate(value, qty) {
+			if (value.length > qty) {
+				return value.substring(0, qty)+"...";	
+			} else {
+				return value;
+			}
+		},
+
+		currentConverter(value) {
+			if(value) {
+				let amount = 'R$ '+value.amount.toFixed(2).toString().replace('.',',');
+				return amount;
+			} else {
+				return "Produto Indisponível";
+			}
 			
 		}
-	},
-	props: ['title', 'description', 'price', 'img']
+	}
+	
 }
 </script>
 
 <style lang="sass?outputStyle=expanded">
+	/* Colours */
+	$text-color: #003920;
 	.shelf {
-		border: 1px solid red;
+		border: 1px solid lighten($text-color, 75%);
 		display: inline-block;
-		width: 30%;
-		height: 275px;
+		width: 25%;
 		margin: 0 1% 1% 0;
 		vertical-align: top;
 		box-sizing: border-box;
+		padding: 10px;
 		&__img {
 			display: inline-block;
+			width: 30%;
+			float: left;
+			img {
+				width: 100%;
+			}
 		}
 
 		&__content {
@@ -42,6 +64,8 @@ export default {
 			width: 60%;
 			margin-left: 10px;
 			text-align: left;
+    		box-sizing: border-box;
+			color: $text-color;
 		}
 
 		&__title {
@@ -51,9 +75,11 @@ export default {
 		}
 		&__description {
 			margin: 5px 0;
-			height: 50px;
-			overflow: hidden;
-			text-overflow: ellipsis;
+			font-size: 14px;
+		}
+		&__price {
+			font-weight: 600;
+			color: #0db14b;
 		}
 	}
 </style>
