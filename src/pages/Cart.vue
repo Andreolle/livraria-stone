3f5552c8837ecae861954292e975b5bffbb0798e
Andreolle Dantas <andreolle18@gmail.com>
2017-11-20 20:50:13 -0200

    criando o template basico para quando o carrinho estiver vazio

--- src/pages/Cart.vue
+++ src/pages/Cart.vue
@@ -2,27 +2,36 @@
   <div>
     <section class="page-cart">
         <div class="container">
-            <table class="cart-list">
-                <thead>
-                    <tr>
-                        <th>Produto</th>
-                        <th>Preço</th>
-                        <th>Remover</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <template v-for="item of getCart">
-                        <cart-item
-                        :title="item.volumeInfo.title"
-                        :description="item.volumeInfo.description"
-                        :price="item.saleInfo.listPrice"
-                        :img="item.volumeInfo.imageLinks.thumbnail"
-                        :id="item.id"
-                        ></cart-item>
-                    </template>
-                </tbody>
-            </table>
-            <div class="totalize"><strong>TOTAL:</strong> <span class="total">R$ {{ amount }}</span></div>
+            <template v-if="getCart.length == '0'">
+                <div class="empty-cart">
+                    <h2 class="empty-cart__title">Seu carrinho está vazio.</h2>
+                    <p>Para continuar comprando, navegue pelo site ou faça uma busca pelo seu produto.</p>
+                    <router-link to="/" class="btn-back">Voltar </router-link>
+                </div>
+            </template>
+            <template v-else>
+                <table class="cart-list">
+                    <thead>
+                        <tr>
+                            <th>Produto</th>
+                            <th>Preço</th>
+                            <th>Remover</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <template v-for="item of getCart">
+                            <cart-item
+                            :title="item.volumeInfo.title"
+                            :description="item.volumeInfo.description"
+                            :price="item.saleInfo.listPrice"
+                            :img="item.volumeInfo.imageLinks.thumbnail"
+                            :id="item.id"
+                            ></cart-item>
+                        </template>
+                    </tbody>
+                </table>
+                <div class="totalize"><strong>TOTAL:</strong> <span class="total">R$ {{ amount }}</span></div>
+            </template>
         </div>
     </section>
   </div>
@@ -104,6 +113,29 @@
         .total {
             font-size: 24px;
             margin-left: 10px;
+        }
+    }
+
+    .empty-cart {
+        color: #003920;
+        &__title {
+            font-size: 26px;
+            margin-bottom: 10px;
+        }
+        p {
+            margin: 5px 0 25px 0;
+        }
+        .btn-back {
+            display: inline-block;
+            text-transform: uppercase;
+            background: #95c93f;
+            color: #ffffff;
+            text-decoration: none;
+            padding: 10px 15px;
+            box-sizing: border-box;
+            &:hover {
+                background: darken(#95c93f, 12%);
+            }
         }
     }
 
